84d0d1ee3795c699239d064ae5f77689a04130af
damini-niranjan <daminin98@gmail.com>
2021-12-17 23:51:39 +0530

    Formatted categories

--- blog_application/lib/screens/categories/FashionScreen.dart
+++ blog_application/lib/screens/categories/FashionScreen.dart
@@ -64,6 +64,9 @@ class FashionDetailScreen extends StatelessWidget {
                                       children: [
                                         Text(
                                           data['title'],
+                                            style: TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.bold)
                                         )
                                       ],
                                     ),
@@ -87,7 +90,9 @@ class FashionDetailScreen extends StatelessWidget {
                                                 0,
                                                 8),
                                             child: Text(data[
-                                            'description']),
+                                            'description'],
+                                                style: TextStyle(
+                                                    fontSize: 14)),
                                           ),
                                         )
                                       ],
@@ -139,6 +144,8 @@ class FashionDetailScreen extends StatelessWidget {
                                               child: Text(
                                                 data[
                                                 'category'],
+                                                  style: TextStyle(
+                                                      fontSize: 12),
                                               ),
                                             )
                                           ],
